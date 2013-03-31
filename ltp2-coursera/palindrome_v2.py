1bb94ecf7cf835b8017e38994e19adebce41ac3a
Sergio Revilla <sergio@unmundotecnologico.org>
2013-03-31 14:00:26 +0200

    second version for palindrome algorithm

--- ltp2-coursera/palindrome_v2.py
+++ ltp2-coursera/palindrome_v2.py
@@ -1,4 +1,4 @@
-def is_palindrome_v1(s):
+def is_palindrome_v2(s):
     """(str) -> bool
 
     Return True if and only if s is a palindrome.
@@ -9,3 +9,24 @@ def is_palindrome_v1(s):
     >>> is_palindrome(dented)
     False
     """
+    n = len(s)
+    return s[:n // 2] == reverse(s[n - n // 2:])
+    
+def reverse(s):
+    """
+    
+    Returns a reversed version of s
+    
+    >>> reverse('hello')
+    'olleh'
+    >>> reverse('a')
+    'a'
+    """
+    
+    reversed = ''
+    for character in s:
+        reversed = character + reversed
+    
+    return reversed
+
+print is_palindrome_v2('hello')
